6186be061c695154bcb4f700e020a8dbbe8ff001
am-abudu <Linqi070905>
2020-03-23 01:55:11 +0000

    Update

---
+++ js/gallery.js
@@ -0,0 +1,3 @@
+// build time:Mon Mar 23 2020 01:55:10 GMT+0000 (Coordinated Universal Time)
+document.addEventListener("DOMContentLoaded",function(){if(typeof $.fn.lightGallery==="function"){$(".article").lightGallery({selector:".gallery-item"})}if(typeof $.fn.justifiedGallery==="function"){if($(".justified-gallery > p > .gallery-item").length){$(".justified-gallery > p > .gallery-item").unwrap()}$(".justified-gallery").justifiedGallery()}});
+//rebuild by neat 
